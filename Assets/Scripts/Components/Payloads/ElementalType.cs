623378397f3cbd370b874f5689b9857139c27c9c
Unknown Studios <eric.gomes@unknownstudios.com.br>
2025-10-24 19:05:10 -0300

    feat(payloads): add payload components and buffer system (#5)
    Implements DynamicBuffer<PayloadElement> system for spell effects.
    Defines PayloadElement struct with Type, Element, Effect, Amount, and Duration fields.
    
    - Add PayloadType enum (Damage, Heal, ApplyStatusEffect, SpawnEntity)
    - Add ElementalType enum (None, Fire, Frost, Shock, Physical, Holy)
    - Add StatusEffectType enum (None, Slow, Burn, Stun, Poison)
    - Add PayloadElement struct implementing IBufferElementData
    
    Implements: SPWT-4
    Part of: SPWS-1

---
+++ Assets/Scripts/Components/Payloads/ElementalType.cs
@@ -0,0 +1,12 @@
+namespace Spellwright.Components.Payloads
+{
+    public enum ElementalType : byte
+    {
+        None = 0,
+        Fire = 1,
+        Frost = 2,
+        Shock = 3,
+        Physical = 4,
+        Holy = 5
+    }
+}
